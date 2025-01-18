b09d12b693a9d122831b2ede8d4d8c69457a7d04
Jared Enck <jaredenck86@gmail.com>
2025-01-18 14:50:05 -0500

    add material-ui packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
